aa71348c3c9cc6b6d4b3d03a3636436af7f23249
Aneesh Agrawal <aneeshusa@gmail.com>
2015-04-21 03:33:49 -0400

    Remove db, update event name requirements.

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,7 @@ class SignupForm(Form):
     publicKey = TextField(validators=[InputRequired()]) # Add Length and Regexp as required
 
 class AddEventForm(Form):
-    name = TextField(validators=[InputRequired(), Length(min=1, max=30), Regexp("^[a-zA-Z0-9]+$")])
+    name = TextField(validators=[InputRequired(), Length(min=2, max=30), Regexp("^[a-zA-Z0-9][a-zA-Z0-9 ]*[a-zA-Z0-9]+$")])
     start = DateTimeField(format='%Y-%m-%dT%H:%M:%S', validators=[InputRequired()])
     end = DateTimeField(format='%Y-%m-%dT%H:%M:%S', validators=[InputRequired()])
     description = TextField(validators=[InputRequired(), Length(min=0, max=1000)])
